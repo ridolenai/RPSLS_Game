d701b8130f31709b6c8caa8f6bdff0fc686cb0d1
ridolenai <92106359+ridolenai@users.noreply.github.com>
2021-12-01 14:17:12 -0600

    Commit to store

--- ai.py
+++ ai.py
@@ -4,7 +4,7 @@ from random import choice
 gesture_options = ['Rock', 'Paper', 'Scissors', 'Lizard', 'Spock']
 
 class AI(Player):
-    gesture_options = ['Rock', 'Paper', 'Scissors', 'Lizard', 'Spock']
+    
     def __init__(self):
         self.name = 'Hal'
         self.score = 0
